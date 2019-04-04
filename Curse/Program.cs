5b8df471298753c6fdbe4c6dcc1971d65e011f21
Alex Lima <alex.luiz.lima@gmail.com.br>
2019-04-04 10:40:33 -0300

    Acrescentada a mensagem Boa noite

--- Curse/Program.cs
+++ Curse/Program.cs
@@ -12,6 +12,7 @@ namespace Curse
         {
             Console.WriteLine("Bom dia!");
             Console.WriteLine("Boa tarde!");
+            Console.WriteLine("Boa noite!");
         }
     }
 }
